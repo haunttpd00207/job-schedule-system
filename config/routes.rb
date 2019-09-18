687b9403673817dc92c516bc6646b77d34e30b2a
Hau Nguyen <haunttpd00207@gmail.com>
2019-09-18 10:34:52 +0700

    user_basic_function

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
+<<<<<<< HEAD
   resources :tasks
   resources :recurring_tasks
+=======
+>>>>>>> user_basic_function
   devise_for :users
 end
