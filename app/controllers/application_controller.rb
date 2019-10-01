7fb0e67365c4b18605bd2f7bb6fd5c3e0011f09d
BlackPearl96 <van9665@gmail.com>
2019-10-01 15:16:10 +0700

    send report to slack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
+  include ReportsHelper
+
   protected
 
   def configure_permitted_parameters
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,13 +2,14 @@
 
 class ReportsController < ApplicationController
   def index
-    @rows = current_user.reports.includes(:task, :user)
+    @rows = current_user.reports.includes(:task, :user).select { |report| report.created_at.to_date == Date.today }
     @reports = Kaminari.paginate_array(@rows).page(params[:page]).per(5)
   end
 
   def create
-    @user = User.find params[:user_id]
-    SendEmailJob.delay(run_at: 20.seconds.from_now).perform_later(@user)
+    @reports = current_user.reports.where("created_at >= ?", Time.now.beginning_of_day)
+    # SendEmailJob.delay(run_at: 20.seconds.from_now).perform_later(current_user)
+    call_api_slack @reports
     redirect_to reports_path
   end
 end
