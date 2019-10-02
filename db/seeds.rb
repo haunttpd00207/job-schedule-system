66324034e8409d5c5d5057b2f2f9d6384d2873e4
haunttpd00207 <53610460+haunttpd00207@users.noreply.github.com>
2019-10-02 08:33:29 +0700

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-passowrd = "123123"
-admin = User.create(username: "Admin" , email: "admin@gmail.com" , password: passowrd , password_confirmation: passowrd, role: 2)
-leader = User.create(username: "Leader" , email: "leader@gmail.com" , password: passowrd , password_confirmation: passowrd, role: 1)
-user = User.create(username: "User" , email: "user@gmail.com" , password: passowrd , password_confirmation: passowrd)
+password = "123123"
+admin = User.create(username: "Admin" , email: "admin@gmail.com" , password: password , password_confirmation: password, role: 2)
+leader = User.create(username: "Leader" , email: "leader@gmail.com" , password: password , password_confirmation: password, role: 1)
+user = User.create(username: "User" , email: "user@gmail.com" , password: password , password_confirmation: password)
